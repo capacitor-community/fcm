43a8f3e87917026bd972439ed61aadffc071c210
lights0123 <developer@lights0123.com>
2020-03-20 10:45:38 -0400

    Add AndroidX compatibility

--- android/capacitor-fcm/src/main/java/io/stewan/capacitor/fcm/FCMPlugin.java
+++ android/capacitor-fcm/src/main/java/io/stewan/capacitor/fcm/FCMPlugin.java
@@ -1,16 +1,11 @@
 package io.stewan.capacitor.fcm;
 
-import android.support.annotation.NonNull;
-
 import com.getcapacitor.JSObject;
 import com.getcapacitor.NativePlugin;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.iid.FirebaseInstanceId;
-import com.google.firebase.iid.InstanceIdResult;
 import com.google.firebase.messaging.FirebaseMessaging;
 
 import java.io.IOException;
@@ -31,20 +26,12 @@ public class FCMPlugin extends Plugin {
         FirebaseMessaging
                 .getInstance()
                 .subscribeToTopic(topicName)
-                .addOnSuccessListener(new OnSuccessListener<Void>() {
-                    @Override
-                    public void onSuccess(Void aVoid) {
-                        JSObject ret = new JSObject();
-                        ret.put("message", "Subscribed to topic " + topicName);
-                        call.success(ret);
-                    }
+                .addOnSuccessListener(aVoid -> {
+                    JSObject ret = new JSObject();
+                    ret.put("message", "Subscribed to topic " + topicName);
+                    call.success(ret);
                 })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        call.error("Cant subscribe to topic" + topicName, e);
-                    }
-                });
+                .addOnFailureListener(e -> call.error("Cant subscribe to topic" + topicName, e));
 
     }
 
@@ -55,34 +42,24 @@ public class FCMPlugin extends Plugin {
         FirebaseMessaging
                 .getInstance()
                 .unsubscribeFromTopic(topicName)
-                .addOnSuccessListener(new OnSuccessListener<Void>() {
-                    @Override
-                    public void onSuccess(Void aVoid) {
-                        JSObject ret = new JSObject();
-                        ret.put("message", "Unsubscribed from topic " + topicName);
-                        call.success(ret);
-                    }
+                .addOnSuccessListener(aVoid -> {
+                    JSObject ret = new JSObject();
+                    ret.put("message", "Unsubscribed from topic " + topicName);
+                    call.success(ret);
                 })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        call.error("Cant unsubscribe from topic" + topicName, e);
-                    }
-                });
+                .addOnFailureListener(e -> call.error("Cant unsubscribe from topic" + topicName, e));
 
     }
 
     @PluginMethod()
     public void deleteInstance(final PluginCall call) {
-        Runnable r = new Runnable() {
-            public void run() {
-                try {
-                    FirebaseInstanceId.getInstance().deleteInstanceId();
-                    call.success();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                    call.error("Cant delete Firebase Instance ID", e);
-                }
+        Runnable r = () -> {
+            try {
+                FirebaseInstanceId.getInstance().deleteInstanceId();
+                call.success();
+            } catch (IOException e) {
+                e.printStackTrace();
+                call.error("Cant delete Firebase Instance ID", e);
             }
         };
 
@@ -93,18 +70,11 @@ public class FCMPlugin extends Plugin {
 
     @PluginMethod()
     public void getToken(final PluginCall call) {
-        FirebaseInstanceId.getInstance().getInstanceId().addOnSuccessListener(getActivity(),  new OnSuccessListener<InstanceIdResult>() {
-            @Override
-            public void onSuccess(InstanceIdResult instanceIdResult) {
-                JSObject data = new JSObject();
-                data.put("token", instanceIdResult.getToken());
-                call.success(data);
-            }
+        FirebaseInstanceId.getInstance().getInstanceId().addOnSuccessListener(getActivity(), instanceIdResult -> {
+            JSObject data = new JSObject();
+            data.put("token", instanceIdResult.getToken());
+            call.success(data);
         });
-        FirebaseInstanceId.getInstance().getInstanceId().addOnFailureListener(new OnFailureListener() {
-            public void onFailure(Exception e) {
-                call.error("Failed to get instance FirebaseID", e);
-            }
-        });
+        FirebaseInstanceId.getInstance().getInstanceId().addOnFailureListener(e -> call.error("Failed to get instance FirebaseID", e));
     }
 }
