e6f7756189f04295df55a7b3d77826eab2406bd2
Hemang Kumar <hemangsk@gmail.com>
2021-06-11 15:28:03 +0530

    plugin: Remove file and move docs to definition.ts

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,14 +1,43 @@
-declare module '@capacitor/core' {
-  interface PluginRegistry {
-    FCMPlugin: FCMProtocol;
-  }
-}
-
-export interface FCMProtocol {
+export interface FCMPlugin {
+  /**
+   * Subscribe to fcm topic
+   * @param options
+   */
   subscribeTo(options: { topic: string }): Promise<{ message: string }>;
+
+  /**
+   * Unsubscribe from fcm topic
+   * @param options
+   */
   unsubscribeFrom(options: { topic: string }): Promise<{ message: string }>;
+
+  /**
+   * Get fcm token to eventually use from a serve
+   *
+   * Recommended to use this instead of
+   * @usage
+   * ```typescript
+   * PushNotifications.addListener("registration", (token) => {
+   *   console.log(token.data);
+   * });
+   * ```
+   * because the native capacitor method, for apple, returns the APN's token
+   */
   getToken(): Promise<{ token: string }>;
+
+  /**
+   * Remove local fcm instance completely
+   */
   deleteInstance(): Promise<boolean>;
+
+  /**
+   * Enabled/disabled auto initialization.
+   * @param options
+   */
   setAutoInit(options: { enabled: boolean }): Promise<void>;
+
+  /**
+   * Retrieve the auto initialization status.
+   */
   isAutoInitEnabled(): Promise<{ enabled: boolean }>;
 }
