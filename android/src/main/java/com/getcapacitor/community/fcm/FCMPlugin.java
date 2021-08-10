18bb0a0b33bd4ed71b9b990a0153f9d06541003b
halomademeapc <alex@halomademeapc.com>
2021-08-10 13:15:42 -0400

    update android firebase dep to version 22

--- android/src/main/java/com/getcapacitor/community/fcm/FCMPlugin.java
+++ android/src/main/java/com/getcapacitor/community/fcm/FCMPlugin.java
@@ -1,20 +1,17 @@
 package com.getcapacitor.community.fcm;
 
 import com.getcapacitor.JSObject;
-import com.getcapacitor.NativePlugin;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
 import com.getcapacitor.annotation.CapacitorPlugin;
-import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.installations.FirebaseInstallations;
 import com.google.firebase.messaging.FirebaseMessaging;
-
-import java.io.IOException;
 
 /**
  * Please read the Capacitor Android Plugin Development Guide
  * here: https://capacitor.ionicframework.com/docs/plugins/android
- *
+ * <p>
  * Created by Stewan Silva on 1/23/19.
  */
 @CapacitorPlugin(
@@ -56,29 +53,22 @@ public class FCMPlugin extends Plugin {
 
     @PluginMethod()
     public void deleteInstance(final PluginCall call) {
-        Runnable r = () -> {
-            try {
-                FirebaseInstanceId.getInstance().deleteInstanceId();
-                call.resolve();
-            } catch (IOException e) {
-                e.printStackTrace();
-                call.reject("Cant delete Firebase Instance ID", e);
-            }
-        };
-
-        // Run in background thread since `deleteInstanceId()` is a blocking request.
-        // See https://firebase.google.com/docs/reference/android/com/google/firebase/iid/FirebaseInstanceId#deleteInstanceId()
-        new Thread(r).start();
+        FirebaseInstallations.getInstance().delete()
+                .addOnSuccessListener(aVoid -> call.resolve())
+                .addOnFailureListener(e -> {
+                    e.printStackTrace();
+                    call.reject("Cant delete Firebase Instance ID", e);
+                });
     }
 
     @PluginMethod()
     public void getToken(final PluginCall call) {
-        FirebaseInstanceId.getInstance().getInstanceId().addOnSuccessListener(getActivity(), instanceIdResult -> {
+        FirebaseInstallations.getInstance().getToken(false).addOnSuccessListener(getActivity(), instanceIdResult -> {
             JSObject data = new JSObject();
             data.put("token", instanceIdResult.getToken());
             call.resolve(data);
         });
-        FirebaseInstanceId.getInstance().getInstanceId().addOnFailureListener(e -> call.reject("Failed to get instance FirebaseID", e));
+        FirebaseInstallations.getInstance().getId().addOnFailureListener(e -> call.reject("Failed to get instance FirebaseID", e));
     }
 
     @PluginMethod()
