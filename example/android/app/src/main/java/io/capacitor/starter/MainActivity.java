f1e33bb9f405e7f1ad1675eac7d9135707ce7406
stewones <oi@stewan.io>
2021-09-13 18:35:23 -0300

    chore(): fix demo app

--- example/android/app/src/main/java/io/capacitor/starter/MainActivity.java
+++ example/android/app/src/main/java/io/capacitor/starter/MainActivity.java
@@ -1,7 +1,5 @@
 package io.capacitor.starter;
 
-import com.getcapacitor.community.fcm.FCMPlugin;
-
 import android.os.Bundle;
 
 import com.getcapacitor.BridgeActivity;
@@ -9,16 +7,4 @@ import com.getcapacitor.Plugin;
 
 import java.util.ArrayList;
 
-public class MainActivity extends BridgeActivity {
-  @Override
-  public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-
-    // Initializes the Bridge
-    this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
-      // Additional plugins you've installed go here
-      // Ex: add(TotallyAwesomePlugin.class);
-      add(FCMPlugin.class);
-    }});
-  }
-}
+public class MainActivity extends BridgeActivity {}
