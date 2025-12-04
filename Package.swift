bdfc5d7b669156be8483df03144aee7d4c47a45d
tompidom <47992794+tompidom@users.noreply.github.com>
2025-12-04 16:01:54 +0100

    feat: iOS Swift Package definition (#182)

---
+++ Package.swift
@@ -0,0 +1,46 @@
+// swift-tools-version: 5.9
+import PackageDescription
+
+let package = Package(
+    name: "CapacitorCommunityFcm",
+    platforms: [.iOS(.v15)],
+    products: [
+        .library(
+            name: "CapacitorCommunityFcm",
+            targets: ["CapacitorCommunityFcmC", "CapacitorCommunityFcmSwift"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "7.0.0"),
+        .package(url: "https://github.com/firebase/firebase-ios-sdk.git", from: "12.6.0"),
+    ],
+    targets: [
+        .target(
+            name: "CapacitorCommunityFcmC",
+            dependencies: [
+                .product(name: "Capacitor", package: "capacitor-swift-pm")
+            ],
+            path: "ios/Plugin",
+            sources: [
+                "Plugin.h",
+                "Plugin.m"
+            ],
+            publicHeadersPath: "."
+        ),
+        .target(
+            name: "CapacitorCommunityFcmSwift",
+            dependencies: [
+                .product(name: "Capacitor", package: "capacitor-swift-pm"),
+                .product(name: "FirebaseMessaging", package: "firebase-ios-sdk")
+            ],
+            path: "ios/Plugin",
+            sources: [
+                "Plugin.swift"
+            ]
+        ),
+        .testTarget(
+            name: "CapacitorCommunityFcmTests",
+            dependencies: ["CapacitorCommunityFcmSwift"],
+            path: "ios/PluginTests"
+        )
+    ]
+)
